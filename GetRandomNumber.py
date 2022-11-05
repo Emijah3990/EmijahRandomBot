1c5dd9c416f93ccc06bde894d85deb230cfe5a79
Emijah3990 <emijah_white@proton.me>
2022-11-06 00:48:24 +0100

    Creating python file for getting random numbers

---
+++ GetRandomNumber.py
@@ -0,0 +1,13 @@
+import random
+
+def random_100():
+    random_number = random.randint(1, 100)
+    return random_number
+
+def random_1000():
+    random_number = random.randint(1, 1000)
+    return random_number
+
+def random_number(first_num, second_num):
+    random_number = random.randint(first_num, second_num)
+    return random_number
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot(API_TOKEN)
 
 
 # Handles '/start', 'help'...
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_message(message):
     mess = f"""
         Hi {message.from_user.first_name} {message.from_user.last_name}!,
@@ -16,8 +16,19 @@ def send_message(message):
         I'm offering random numbers to anyone on the telegram.
         Use /help to get list of available commands.
         """
-
     bot.send_message(message.chat.id, mess, parse_mode='html')
+
+@bot.message_handler(commands=['help'])
+def send_message(message):
+    help_message = '''
+    This telegram bot will give u random number from any range u pick.
+    USE:
+    /random_number_100 to get random number from 1 to 100
+    /random_number_1000 to get number from 1 to 1000
+    /random_number to get random number in your range
+    '''
+    bot.send_message(message.chat.id, help_message, parse_mode='html')
+
 
 
 bot.infinity_polling()
