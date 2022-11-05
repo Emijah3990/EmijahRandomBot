16a452f39ad25796b689d3f90c2f131ebd64ec15
Emijah3990 <emijah_white@proton.me>
2022-11-06 00:20:48 +0100

    reformatting the file v.1

--- main.py
+++ main.py
@@ -2,19 +2,22 @@
 
 import telebot
 
-API_TOKEN ='5609895910:AAGgizUnGZayS13VMfvuwpWfmt8HE2QqN0k'
+API_TOKEN = '5609895910:AAGgizUnGZayS13VMfvuwpWfmt8HE2QqN0k'
 
 bot = telebot.TeleBot(API_TOKEN)
 
-#Handles '/start', 'help'...
+
+# Handles '/start', 'help'...
 @bot.message_handler(commands=['start', 'help'])
 def send_message(message):
-    bot.send_message(message.chat.id, """
-        <b>Hi everyone, I'm Emijah_Random_Bot.
+    mess = f"""
+        Hi {message.from_user.first_name} {message.from_user.last_name}!,
+        I'm Emijah_Random_Bot.
         I'm offering random numbers to anyone on the telegram.
-        </b>""", parse_mode='html' )
+        Use /help to get list of available commands.
+        """
 
-
+    bot.send_message(message.chat.id, mess, parse_mode='html')
 
 
 bot.infinity_polling()
