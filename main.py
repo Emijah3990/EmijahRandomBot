4573f7084afb7351362b4a9c747959035313b908
Emijah3990 <emijah_white@proton.me>
2022-11-06 00:10:10 +0100

    Creating first function

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ API_TOKEN ='5609895910:AAGgizUnGZayS13VMfvuwpWfmt8HE2QqN0k'
 
 bot = telebot.TeleBot(API_TOKEN)
 
+#Handles '/start', 'help'...
+@bot.message_handler(commands=['start', 'help'])
+def send_message(message):
+    bot.send_message(message.chat.id, """
+        <b>Hi everyone, I'm Emijah_Random_Bot.
+        I'm offering random numbers to anyone on the telegram.
+        </b>""", parse_mode='html' )
 
 
 
+
+bot.infinity_polling()
